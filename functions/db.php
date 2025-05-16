940482743ea99476f398be2f551e8f1cfafc1203
1Stick1 <84839119+1Stick1@users.noreply.github.com>
2025-05-16 15:32:26 +0300

    Update db.php

--- functions/db.php
+++ functions/db.php
@@ -1,23 +1,13 @@
 <?php
-// Получаем строку подключения из переменной окружения
-$dbUrl = getenv('mysql://root:KIZQusulaxPQkezVyzmUQAIQBPDOIhfr@yamabiko.proxy.rlwy.net:35387/railway');  // В зависимости от твоей базы, это может быть DATABASE_URL для PostgreSQL или CLEARDB_DATABASE_URL для MySQL
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "piekarnia";
 
-// Разбираем строку на компоненты
-$url = parse_url($dbUrl);
+$conn = new mysqli($host, $user, $password, $dbname);
 
-// Извлекаем параметры подключения из строки
-$host = $url['host'];
-$user = $url['user'];
-$password = $url['pass'];
-$dbname = ltrim($url['path'], '/');  // Убираем слэш в начале пути, который указывает на имя базы данных
-
-// Создаем подключение
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-// Проверка на успешное подключение
+// Проверка соединения
 if ($conn->connect_error) {
-    die("Connect error: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connected successfully";
 ?>
